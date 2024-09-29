c6af1a4b139a5bee6a1443d656260635fa7a7541
Unurjargal Gantumur <unurjargal2001@gmail.com>
2024-09-29 15:36:16 -0400

    Benchmark Test

---
+++ src/benchmark_example.cpp
@@ -0,0 +1,13 @@
+#include <benchmarks/benchmark.h>
+#include "my_add.h"
+
+void Sum(int n) {
+    int sum_ = 0;
+    for (int i = 1; i <= n; ++i) {
+        sum_ += i;
+    }
+}
+
+BENCHMARK(BM_Sum)
+
+BENCHMARK_MAIN();
