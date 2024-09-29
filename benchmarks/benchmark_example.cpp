53d76669cec191bca00a3d22b93192b99905ee0b
Unurjargal Gantumur <unurjargal2001@gmail.com>
2024-09-29 16:04:09 -0400

    Benchmark Test

--- benchmarks/benchmark_example.cpp
+++ benchmarks/benchmark_example.cpp
@@ -1,13 +1,16 @@
 #include <benchmark/benchmark.h>
 #include "my_add.h"
 
-void Sum(int n) {
-    int sum_ = 0;
-    for (int i = 1; i <= n; ++i) {
-        sum_ += i;
+static void BM_Sum(benchmark::State& state) {
+    for (auto _ : state) {
+        int n = state.range(0);
+        int sum = 0;
+        for (int i = 1; i <= n; ++i) {
+            sum += i;
+        }
     }
 }
 
-BENCHMARK(Sum)
+BENCHMARK(BM_Sum)->Range(8, 8<<10);
 
 BENCHMARK_MAIN();
